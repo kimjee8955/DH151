328496305d0694c7b9f3d42e97d4102e832e0055
kimjee8955 <kimjee8955@gmail.com>
2021-04-12 19:40:23 -0700

    lab

--- Week3/js/map.js
+++ Week3/js/map.js
@@ -1,52 +1,52 @@
 let locations = [
 	{
-		'title':'Osaka',
-		'lat': 34.6937,
-		'lon': 135.5023,
-		'desc': 'My family lived here for 7 years'
+		'title':'Gwangju, Korea',
+		'lat': 35.166668,
+		'lon': 126.916664,
+		'desc': 'I was born here'
 	},
 	{
-		'title':'Los Angeles',
-		'lat': 34.0522,
-		'lon': -118.2437,
-		'desc': 'I go to university here'
-	},
-	{
-		'title':'San Jose',
-		'lat': 37.335480,
-		'lon': -121.893028,
-		'desc': 'I spent my teenage years here'
-	},
-	{
-		'title':'Atlanta',
-		'lat': 33.748783,
-		'lon': -84.388168,
-		'desc': 'My mom works here'
-	},
-	{
-		'title':'Tokyo',
+		'title':'Tokyo, Japan',
 		'lat': 35.658581,
 		'lon': 139.745438,
 		'desc': 'My brother was born here'
 	},
 	{
-		'title':'Cairo',
-		'lat': 30.005493,
-		'lon': 31.477898,
-		'desc': 'My dad worked here'
+		'title':'Osaka, Japan',
+		'lat': 34.6937,
+		'lon': 135.5023,
+		'desc': 'My family lived here for 7 years'
 	},
 	{
-		'title':'Yongin',
+		'title':'Yongin, Korea',
 		'lat': 37.2333,
 		'lon': 127.2000,
 		'desc': 'My family lived here for 4 years'
 	},
 	{
-		'title':'Gwangju',
-		'lat': 35.166668,
-		'lon': 126.916664,
-		'desc': 'I was born here'
+		'title':'San Jose, U.S.A',
+		'lat': 37.335480,
+		'lon': -121.893028,
+		'desc': 'I spent my teenage years here'
 	},
+	{
+		'title':'Los Angeles, U.S.A',
+		'lat': 34.0522,
+		'lon': -118.2437,
+		'desc': 'I go to university here'
+	},
+	{
+		'title':'Atlanta, U.S.A',
+		'lat': 33.748783,
+		'lon': -84.388168,
+		'desc': 'My mom works here'
+	},
+	{
+		'title':'Cairo, Egypt',
+		'lat': 30.005493,
+		'lon': 31.477898,
+		'desc': 'My dad worked here'
+	}
 ];
 /*change icon color to violet*/
 var violetIcon = new L.Icon({
@@ -92,4 +92,13 @@ function flyToIndex(index){
 	map.flyTo([locations[index].lat,locations[index].lon],12)
 	// open the popup
 	myMarkers.getLayers()[index].openPopup()
+}
+
+function ShowAndHide() {
+    var x = document.getElementById('SectionName');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
 }
